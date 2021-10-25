4758b6810b60d90e0a3bb30114c126472820e392
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2021-10-25 16:56:46 +0530

    Delete src directory

--- src/footer.js
+++
@@ -1,7 +0,0 @@
-
-const Footer = () =>{
-    return (
-        <p className="text-white bg-dark text-center text-muted" style={{padding: "60px",color:"yellowgreen"}}><h2>Thanks for shopping</h2></p>
-    );
-};
-export default Footer;
