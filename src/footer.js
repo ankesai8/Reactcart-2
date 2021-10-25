80035ad54346d959058ec0c26b90fe16ac808e65
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2021-10-25 12:25:17 +0530

    Add files via upload

---
+++ src/footer.js
@@ -0,0 +1,7 @@
+
+const Footer = () =>{
+    return (
+        <p className="text-white bg-dark text-center text-muted" style={{padding: "60px",color:"yellowgreen"}}><h2>Thanks for shopping</h2></p>
+    );
+};
+export default Footer;
