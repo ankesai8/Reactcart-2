73bede88719db834de1a098aba5fae73c224c110
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2021-10-25 17:33:23 +0530

    Update header.js

--- src/header.js
+++ src/header.js
@@ -8,13 +8,14 @@ const Header = (props) => {
       <i href="#"class="navbar-brand">Home</i>
       <i href="#"class="navbar-brand"> About</i>
       
-    <select class="form-select" aria-label="Default select example">
-  <option selected>shop</option>
-  <option value="1">All products</option>
-  <option value="2">New arrivals</option>
-  <option value="3">popular items</option>
-</select>
+    <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+
+              <option value="1">All products</option>
+              <option value="2">New arrivals</option>
+              <option value="3">popular items</option>
+    </select>  
      
+
       <button class="addtoc px-4"onClick={props.onclick}><i class="fas fa-shopping-cart"></i><i className="px-2">Cart </i><span className="noofitems bg-dark text-white py-1 px-2">{props.noofitems}</span></button>
       </nav>
       <div className="jumbotron-fluid text-center text-white bg-dark" style={{padding: "90px",fontsize: "500px" }}>
