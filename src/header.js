b486a8e9991e48600848d9637e670ac0c39380b1
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2021-10-25 17:25:33 +0530

    Update header.js

--- src/header.js
+++ src/header.js
@@ -6,14 +6,20 @@ const Header = (props) => {
       <nav class="navbar navbar-dark bg-dark justify-content-around">
 
       <i href="#"class="navbar-brand">Home</i>
-      <i href="#"class="navbar-brand"> Search</i>
-      <i href="#"class="navbar-brand"> Login</i>
-      <i href="#"class="navbar-brand">Shoping.in</i>
+      <i href="#"class="navbar-brand"> About</i>
+      
+    <select class="form-select" aria-label="Default select example">
+  <option selected>shop</option>
+  <option value="1">All products</option></vl>
+  <option value="2">New arrivals</option>
+  <option value="3">popular items</option>
+</select>
+     
       <button class="addtoc px-4"onClick={props.onclick}><i class="fas fa-shopping-cart"></i><i className="px-2">Cart </i><span className="noofitems bg-dark text-white py-1 px-2">{props.noofitems}</span></button>
       </nav>
       <div className="jumbotron-fluid text-center text-white bg-dark" style={{padding: "90px",fontsize: "500px" }}>
-        <p className="display-1" style={{ fontWeight: "Times New Roman",fontsize:"360px" }}><h1>Shop in stores </h1></p>
-        <p className="mutdis text-muted">*-----Shop on your convinence-----*</p>
+        <p className="display-1" style={{ fontWeight: "Times New Roman",fontsize:"360px" }}><h1>Shop in Style </h1></p>
+        <p className="mutdis text-muted">With this shop hompeage template</p>
       </div>
     </>
   );
