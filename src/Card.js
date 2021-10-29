9948bbae67c4c8dbee88278c54e3b4414d9d42df
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2021-10-29 19:16:29 +0530

    Update Card.js

--- src/Card.js
+++ src/Card.js
@@ -7,8 +7,8 @@ const Card = (props) => {
     <div className="col-6 col-md-4 col-lg-3 p-3">
       <div className="cborder mb-5 text-center pb-4">
         <div className="img" style={{ height: 150 }}>450 X 300</div>
-        <h3>{props.details.name}</h3>
-        <h5>{props.details.price}</h5>
+        <h2>{props.details.name}</h2>
+        <h4>{props.details.price}</h4>
         <button className="addtoc btn" disabled ={props.details.disabled === "false" ? false : true} onClick={addtocart}><span>{props.details.disabled === "false" ? "Add to card" : "Added to cart"}</span></button>
       </div>
     </div>
