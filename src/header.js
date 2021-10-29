a3ad1622f2cac61b14d85c2e332e9ee9829e6e98
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2021-10-29 19:08:03 +0530

    Update header.js

--- src/header.js
+++ src/header.js
@@ -5,12 +5,17 @@ const Header = (props) => {
   return (
     <>
       <nav class="navbar navbar-light bg-light justify-content-around">
+        <i href="#"class="navbar-brand">Home</i>
+        <i href="#"class="navbar-brand">About</i>
+        <i href="#"class="navbar-brand">shop</i>
+        
         <i href="#"class="navbar-brand">Shoping.in</i>
+
         <button class="addtoc px-4"onClick={props.onclick}><i class="fas fa-shopping-cart"></i><i className="px-2">Cart </i><span className="noofitems bg-dark text-white py-1 px-2">{props.noofitems}</span></button>
       </nav>
       <div className="jumbotron-fluid text-center text-white bg-dark" style={{ padding: "90px" }}>
         <p className="display-1" style={{ fontWeight: "bold" }}>Shop in Style</p>
-        <p className="mutdis text-white muted">With this shop hompeage template</p>
+        <p className="mutdis text-white">With this shop hompeage template</p>
       </div>
     </>
   );
